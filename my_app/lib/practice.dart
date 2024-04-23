8f0d77ca73be56cdbf9da8214d168034a22f9df5
Rao Umair ALi <106069614+Rao-Umair-Ali@users.noreply.github.com>
2024-04-23 11:40:14 +0500

    class

--- my_app/lib/practice.dart
+++ my_app/lib/practice.dart
@@ -1,6 +1,6 @@
 import 'dart:io';
 void main(){
-//----------------- How showing OutPut--------------------------
+//******************************* How showing OutPut*******************************
 //-----------------  with Print Statement--------------------------
     //print("hello world");
 
@@ -9,12 +9,12 @@ void main(){
     // stdout.write("enter your name");
 
 
-//----------------- How Take InPut--------------------------
+//*******************************How Take InPut*******************************
 //----------------- With stdin Statement--------------------------
     //stdin.readLineSync();
 
 
-//----------------- How store input in Variable --------------------------
+//******************************* How store input in Variable *******************************
  //var name;
  //name = stdin.readLineSync();
 
@@ -24,7 +24,7 @@ void main(){
 //name = stdin.readLineSync();
 //stdout.write("your name is ,$name");
 
-
+//*******************************Concatenation*******************************
 //----------------- How store input in Variable and print and what is this sign " $ "and "+" --------------------------
 //these both sign are used for concatenation
 //concatenation with $
@@ -36,9 +36,39 @@ void main(){
 
 //concatenation with +
 
-var  name;
-name = stdin.readLineSync();
-stdout.write("your name is" +" $name");
+//var  name;
+//name = stdin.readLineSync();
+//stdout.write("your name is" +" $name");
 
 
+//**************************Declaration of variable******************************
+//int number=23;
+//String name="umair";
+
+BigInt a=BigInt.parse(' 654645546445644465654654654');
+
+
+
+//***************************** Class*******************************
+
+
+var Student1=new Human();
+//Student1.number=27;
+//print(Student1.number);
+int c= Student1.sum(9,5);
+print(c);
+
+
+}
+
+
+class Human{
+int number=0;
+String name="umair";
+int sum(int a , int b){
+int sum=a+b;
+return sum;
+
+
+}
 }
